af8567186f880fb650f126a308e489bc5832c703
Panagiotis Rousis <rousis.panagiotis@gmail.com>
2024-10-31 18:29:24 +0200

    More changes to make it work

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { useTranslations } from "next-intl";
-
 export default function Home() {
-  const t = useTranslations("test");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <span className="font-inter text-lg">{t("inter")}</span>
-      <span className="font-outfit text-lg">{t("outfit")}</span>
+      <span className="font-inter text-lg">{"inter font used here..."}</span>
+      <span className="font-outfit text-lg">
+        {"... and outfit font here!!!"}
+      </span>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/waterpath-data-integration-tool",
   output: "export",
   reactStrictMode: true,
 };
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
