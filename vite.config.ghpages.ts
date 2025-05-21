5e97ef453efa81f76ca29b6b800c81e8652897bf
Panagiotis Rousis <rousis.panagiotis@gmail.com>
2025-05-21 09:01:26 +0300

    Add fix for github-pages route

--- vite.config.ghpages.ts
+++ vite.config.ghpages.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.riv'],
-  base: "/waterpath-data-integration-tool/",
+  base: "/waterpath-data-integration-tool",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
